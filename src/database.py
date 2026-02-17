e1462583d21f5399c6fb7a40e6ed99538fa7828b
sirulab <chensiru.sjtu@gmail.com>
2026-02-17 16:07:32 +0800

    prepare for deployment - requirement.txt and db

--- src/database.py
+++ src/database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./database.db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
