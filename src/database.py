d51271c99c94bb0fbb632f2aea775feab7aad2a4
sirulab <chensiru.sjtu@gmail.com>
2026-02-17 16:33:22 +0800

    update Render.com db

--- src/database.py
+++ src/database.py
@@ -7,6 +7,12 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+else:
+    # Render.com PostgreSQL
+    engine = create_engine(DATABASE_URL)
+
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
