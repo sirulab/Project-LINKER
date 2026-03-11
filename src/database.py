fd223019496a46c52a5fea7c7a1937786740d363
sirulab <chensiru.sjtu@gmail.com>
2026-03-11 18:12:06 +0800

    Fix database URL

--- src/database.py
+++ src/database.py
@@ -7,6 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 else:
